3f8d2e3ad84d8ea57bdcb7307bb8f680cfb0abfa
Johnson Emmanuel <76089105+Johnson1425-ux@users.noreply.github.com>
2025-11-30 01:37:15 +0300

    Remove unused import for Analytics
    Removed unused Analytics import from index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { Analytics } from "@vercel/analytics/next"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
